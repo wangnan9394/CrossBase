8ee1a8ccc8f87924a2759629bc6cd5955b918aae
Alan93 <52204680+wangnan9394@users.noreply.github.com>
2021-01-15 14:27:53 +0800

    Update mapping.CONFIG.sh

--- config/mapping.CONFIG.sh
+++ config/mapping.CONFIG.sh
@@ -1,15 +1,15 @@
 ##mapping.CONFIG.sh
 GENOME_PATH=genome_path
 THREADS=threads
-FQ1=fq1
-FQ2=fq2
 SAMPLE_LIST=sample.list
 
 cat $SAMPLE_LIST |while read line
 do
+FQ1=sample_dir/${line}*1*
+FQ2=sample_dir/${line}*2*
 bwa mem -t $THREADS -R "@RG\tID:$line\tLB:$line\tSM:$line\tPL:illumina" $GENOME_PATH $FQ1 $FQ2 | samtools sort -o 0.bam/${line}.sort.bam
 echo "** ${line} BWA MEM done **"
-# 3.≈≈–Ú
+# 3.√Ö√Ö√ê√≤
 gatk MarkDuplicates --REMOVE_DUPLICATES=true -I 0.bam/${line}.sort.bam -M 1.sort.bam/${line}.sort.markdup_metrics.txt -O 1.sort.bam/${line}.sort.removedup.bam
 samtools index 1.sort.bam/${line}.sort.removedup.bam
 echo "** ${line} sorted raw bam file done **"
